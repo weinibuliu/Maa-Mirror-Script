c2e12215b754fd65a180bee72e659a8caddb51ba
不留 <weinibuliu@outlook.com>
2025-01-25 18:55:20 +0800

    chore: 调整超时时间(120->60) ；调整 ISSUE_BODY

--- src/ISSUE_BODY.py
+++ src/ISSUE_BODY.py
@@ -1,4 +1,4 @@
-BODY = """- **[更新来源](https://github.com/MaaAssistantArknights/MaaAssistantArknights/releases/{VERSION})**
+BODY = """# {VERSION}
 
 ## 更新日志 | Release Note
 <details>
@@ -9,10 +9,11 @@ BODY = """- **[更新来源](https://github.com/MaaAssistantArknights/MaaAssista
 
 </details>
 
-### 下载地址 | Download
-详见 **[下载方式]({DOWNLOAD_URL})**
+## 下载地址 | Download
+- **[Maa-Mirror]({DOWNLOAD_URL})**
+- [Github] (https://github.com/MaaAssistantArknights/MaaAssistantArknights/releases/{VERSION})
 
-# 关于 MAA | About MAA
+## 关于 MAA | About MAA
 - [官网](https://maa.plus)
 - [Github](https://github.com/MaaAssistantArknights/MaaAssistantArknights)
 - [用户 QQ 群](https://ota.maa.plus/MaaAssistantArknights/api/qqgroup)
@@ -21,7 +22,6 @@ BODY = """- **[更新来源](https://github.com/MaaAssistantArknights/MaaAssista
 - **Maa-Mirror 与 Maa 无隶属关系，不代表 Maa 开发团队**。
 - **提供国内网盘分流是 Maa-Mirror 自身行为，与 Maa 开发团队无关。**
 - Maa-Mirror 仅提供国内网盘分流，不受理任何 Maa 使用相关问题。
-详见 [关于我们]()
 
 ## 注意事项 | Tips
 - Maa-Mirror 可能会定期删除历史版本以释放可用空间。如有下载特定历史版本的需求，推荐前往 **[Github Release](https://github.com/MaaAssistantArknights/MaaAssistantArknights/releases)** 下载。
